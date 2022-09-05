00306541802894cb1848eb97aaf4c9b4ef79c66c
ELIUS <eliuswanyoro@gmail.com>
2022-09-05 14:37:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 function shout(string) {
     return string.toUpperCase();
 }
-function whisper(string) {
-    return string.toLowerCase();
-}
-function logShout(string){
-    console.log(string.toUpperCase());
-}
-function logWhisper(string){
-    console.log(string.toLowerCase())
+function whisper(string){
+  return string.toLowerCase()
 }
 
-function sayHiToHeadphonedRoommate(string) {
-    var cantUnswer = "I can't hear you!";
-    var yesUnswer = "YES INDEED!";
-    var lovUnswer = "I would love to!";
-    if (string.toLowerCase(string) === string) {
-      return cantUnswer;
-    }
-    else if (string.toUpperCase(string) === string) {
-      return yesUnswer;
-    }
-    else if ("Let`s have dinner together." === "Let`s have dinner together.") {
-      return lovUnswer
-    }
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function  sayHiToHeadphonedRoommate(string){
+   
+  if ( string.toLowerCase(string)=== string){
+    return `I can\'t hear you!`
+  }else if (string.toUpperCase(string) === string){
+    return "YES INDEED!"
   }
+  else if ( `Let\'s have dinner together!`=== `Let\'s have dinner together!` ){
+    return "I would love to!"
+  }
+    
+}
